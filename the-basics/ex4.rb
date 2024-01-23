c39c1b4163a5dcd2b94fd073eff1c7d4364de68c
Sang Pak <spak34@berkeley.edu>
2024-01-23 00:28:38 -0800

    Finished the basics and variables, started on methods.

---
+++ the-basics/ex4.rb
@@ -0,0 +1,2 @@
+movie_years = [1999, 2010, 2009, 2009, 2000]
+puts movie_years
---
+++ variables/name.rb
@@ -0,0 +1,6 @@
+puts "First name please"
+first_name = gets.chomp
+puts "Last name please"
+last_name = gets.chomp
+puts "Hello, " + first_name + " " + last_name
+
